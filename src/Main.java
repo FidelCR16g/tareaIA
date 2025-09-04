cb90c4c53deedfd787379bb64416098acaa88784
fave6 <fidelcr16j@gmail.com>
2025-09-04 13:55:23 -0600

    Primer Comit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Imagínate quedarte estancada con la misma persona a pesar de que te ha puesto cuernos, te ha mentido o te ha humillado");
+        System.out.print("nada más  por que sientes que nadie va a ser capaz de amarte tal como eres.\n");
+        System.out.print("Te deseo lo mejor hoy y siempre, y de todo corazón, si estas leyendo esto");
+        System.out.print("espero que un día te ames tú misma/o lo suficiente como para darte cuenta que no necesitas");
+        System.out.print(" ninguna persona en tú vida y que no hay mejor amor que el que te vas a dar tú misma/o.❤\"");
+    }
+}
